4ba9eeacbe0ee518bfae0834321741bb6774098a
+ <+>
2024-06-04 15:42:59 -0700

    - adds function

---
+++ index.js
@@ -0,0 +1,15 @@
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    // Returns a greeting with name and learning language (defaulting to JavaScript)
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  
+function introduction(name) {
+    // Returns a greeting with the provided name
+    return `Hi, my name is ${name}.`;
+  }
+  
+  function introductionWithLanguage(name, language) {
+    // Returns a greeting with name and learning language
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+  
